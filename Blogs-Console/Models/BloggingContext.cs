cadcabfcbcabefd2b37b6296c883d237f850af2e
jmuckerheide <35705936+jmuckerheide@users.noreply.github.com>
2019-11-04 15:38:06 -0600

    Add files via upload

---
+++ Blogs-Console/Models/BloggingContext.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using NLog;
+namespace BlogsConsole.Models
+{
+    public class BloggingContext : DbContext
+    {
+        private static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+        public BloggingContext() : base("name=BlogContext") { }
+
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+        public void DisplayBlog()
+        {
+            var database = new BloggingContext();
+            var query = database.Blogs.OrderBy(b => b.Name);
+
+            Console.WriteLine($"{query.Count()} Blogs returned");
+            foreach (var item in query)
+            {
+                Console.WriteLine(item.Name);
+            }
+        }
+        public void AddBlog()
+        {
+            Console.Write("Enter new blog");
+            var blog = new Blog { Name = Console.ReadLine() };
+            var database = new BloggingContext();
+        }
+        public void CreatePost()
+        {
+            var database = new BloggingContext();
+            var query = database.Blogs.OrderBy(b => b.BlogId);
+
+            Console.WriteLine("Select blog");
+            foreach (var item in query)
+            {
+                Console.WriteLine($"{item.BlogId}) {item.Name}");
+                Console.WriteLine("Enter blog ID here");
+                int userBlogChosen = int.Parse(Console.ReadLine());
+
+                Post match = new Post();
+
+                if (userBlogChosen == item.BlogId)
+                {
+                    //add post
+                    Console.Write("Enter new post");
+                    var post = new Post { Name = Console.ReadLine() };
+                    var database = new BloggingContext();
+                }
+            }
+        }
+        public void DisplayPost()
+        {
+            var database = new BloggingContext();
+            var query = database.Blogs.OrderBy(b => b.BlogId);
+            Console.WriteLine("Enter Blog ID to display");
+            Console.WriteLine("(D)isplay all blogs");
+            var userSelect = Console.ReadLine().ToUpper();
+
+            foreach (var item in query)
+            {
+                Console.WriteLine($"{item.BlogId}) Posts from {item.Name}");
+            }
+            List<Post> LPost;
+            if (userSelect == "D")
+            {
+                //display all post and blogs
+                Posts = db.Posts.OrderBy(p => p.Title);
+            }
+        }
+        public void DeletePost()
+        {
+            //delete post
+            Console.WriteLine("What post do you wish to delete?");
+            var post = Console.ReadLine();
+            var database = new BloggingContext();
+            database.Remove(post);
+
+        }
+        public void EditPost()
+        {
+            //edit post
+            Console.WriteLine("What post to you wish to edit?");
+            var post = Console.ReadLine();
+            var database = new BloggingContext();
+
+            Posts newPost = post(database);
+
+        }
+    }
+}
---
+++ Blogs-Console/Program.cs
@@ -0,0 +1,74 @@
+﻿using NLog;
+using BlogsConsole.Models;
+using System;
+using System.Linq;
+
+namespace BlogsConsole
+{
+    class MainClass : BloggingContext
+    {
+        private static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+        public static void  Main(string[] args)
+        {
+            logger.Info("Program started");
+            try
+            {
+
+                string userChoice;
+                do
+                {
+                    //ask user for input
+                    Console.WriteLine("1) Display all blogs");
+                    Console.WriteLine("2) Add Blog");
+                    Console.WriteLine("3) Create Post");
+                    Console.WriteLine("4) Display Posts");
+                    Console.WriteLine("5) Delete Post");
+                    Console.WriteLine("6) Edit Post");
+                    Console.WriteLine("Enter x to exit");
+                    userChoice = Console.ReadLine();
+                    //clear user input
+                    Console.Clear();
+                    //log user input
+                    logger.Info("Option {userChoice} selected", userChoice);
+
+                    BloggingContext choice = new BloggingContext();
+                    if (userChoice == "1")
+                    {
+                        //display all blogs
+                        choice.DisplayBlog();
+                    } 
+                    else if (userChoice == "2")
+                    {
+                        //add blog
+                        choice.AddBlog();
+                    }
+                    else if (userChoice == "3")
+                    {
+                        //create post
+                        choice.CreatePost();
+                    }
+                    else if (userChoice == "4")
+                    {
+                        //display post
+                        choice.DisplayPost();
+                    }
+                    else if (userChoice == "5")
+                    {
+                        //delete post
+                        choice.DeletePost();
+                    }
+                    else if (userChoice == "6")
+                    {
+                        //edit post
+                        choice.EditPost();
+                    }
+                } while (userChoice.ToLower() != "x");
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex.Message);
+            }
+            logger.Info("Program ended");
+        }
+    }
+}
